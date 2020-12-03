819426ce085ac24d4cee6c65739cf3d9ae826a57
jaq-cat <jaq.cat@protonmail.ch>
2020-12-03 18:14:29 -0300

    add to tree

--- src/binary_tree.h
+++ src/binary_tree.h
@@ -5,18 +5,34 @@
 #include <stdlib.h>
 
 typedef struct s_BinaryTree {
-    int *data;
+    int data;
     struct s_BinaryTree *left;
     struct s_BinaryTree *right;
 } BinaryTree;
 
-BinaryTree binary_tree() {
+BinaryTree binary_tree(int data) {
     BinaryTree tree = {
-        .data = NULL,
+        .data = data,
         .left = NULL,
         .right = NULL,
     };
     return tree;
+}
+
+void add_to_tree(BinaryTree *tree, int data) {
+    if (data <= tree->data) {
+        if (tree->left == NULL) {
+            tree->left = (BinaryTree*) malloc(sizeof(BinaryTree));
+            *tree->left = binary_tree(data);
+        } else
+            add_to_tree(tree->left, data);
+    } else {
+        if (tree->right == NULL) {
+            tree->right = (BinaryTree*) malloc(sizeof(BinaryTree));
+            *tree->right = binary_tree(data);
+        } else
+            add_to_tree(tree->right, data);
+    }
 }
 
 #endif
