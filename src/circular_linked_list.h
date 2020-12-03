05f557b29e7b9a3c74354979997c73555ac4e687
jaq-cat <jaq.cat@protonmail.ch>
2020-12-03 20:44:26 -0300

    fix stuf

--- src/circular_linked_list.h
+++ src/circular_linked_list.h
@@ -14,7 +14,7 @@ typedef struct s_CLinkedList {
     int len;
 } CLinkedList;
 
-CLinkedList linked_list() {
+CLinkedList c_linked_list() {
     CLinkedList list = {
         .head = NULL,
         .len = 0,
@@ -22,7 +22,7 @@ CLinkedList linked_list() {
     return list;
 }
 
-CLLNode* make_node(int data) {
+CLLNode* make_c_node(int data) {
     CLLNode *node = (CLLNode*) malloc(sizeof(CLLNode));
     node->data = (int*) malloc(sizeof(int));
     *(node->data) = data;
@@ -30,39 +30,16 @@ CLLNode* make_node(int data) {
     return node;
 }
 
-void add_to_list(CLinkedList *list, int data) {
+void add_to_c_list(CLinkedList *list, int data) {
     // empty list
     if (list->len == 0)
-        list->head = make_node(data);
+        list->head = make_c_node(data);
     else {
         CLLNode *node;
         for (node = list->head; node->next != NULL; node = node->next); // move to last node
-        node->next = make_node(data);
+        node->next = make_c_node(data);
     }
     list->len += 1;
-}
-
-void remove_first(CLinkedList *list) {
-    free(list->head->data);
-    list->head = list->head->next;
-}
-
-void remove_node_after(CLLNode *previous) {
-    free(previous->next->data);
-    previous->next = previous->next->next;
-}
-
-void _remove_from_list(int len, CLLNode *node, int i, int c) {
-    if (i-1 == c)
-        remove_node_after(node);
-    else if (i < len)
-        _remove_from_list(len, node->next, i, c+1);
-}
-
-void remove_from_list(CLinkedList *list, int i) {
-    if (list->len == 0) return;
-    if (list->len == 1 || i == 0) remove_first(list);
-    else _remove_from_list(list->len, list->head, i, 0);
 }
 
 void print_list(CLinkedList *list) {
@@ -76,6 +53,7 @@ void print_list(CLinkedList *list) {
 void _delete_node(CLLNode *node) {
     if (node->next != NULL)
         _delete_node(node->next);
+    node->next = NULL;
     free(node->data);
     free(node);
 }
