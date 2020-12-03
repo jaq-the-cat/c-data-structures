f9cef5ecd60781214a874462656a0aac695824fc
jaq-cat <jaq.cat@protonmail.ch>
2020-12-03 12:53:05 -0300

    a

--- src/vector.h
+++ src/vector.h
@@ -7,21 +7,28 @@
 typedef struct s_Vector {
     int *array;
     int length;
+    int allocated;
 } Vector;
 
-Vector vector(const int length) {
-    int *a = (int*) malloc(sizeof(int)*length);
+Vector vector(const int initial_length) {
+    int *a = (int*) malloc(sizeof(int)*initial_length);
     Vector v = Vector {
         .array = a,
-        .length = length,
+        .length = 0,
+        .allocated = initial_length,
     };
-    for (int i=0; i<length; i++)
-        v.array[i] = 0;
     return v;
 }
 
 void set_at(Vector *v, const int i, const int d) {
-    v->array[i] = d;
+    if (i < v->length)
+        v->array[i] = d;
+}
+
+void push(Vector *v, const int d) {
+    if (v->length < v->allocated)
+        v->array[v->length] = d;
+    v->length += 1;
 }
 
 void print_vec(const Vector *v) {
