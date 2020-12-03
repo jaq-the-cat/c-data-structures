31434d82cc487dcdb56a5fb61a74c3903b730f30
jaq-cat <jaq.cat@protonmail.ch>
2020-12-03 11:33:56 -0300

    i think that works?

---
+++ src/linked_list.h
@@ -0,0 +1,32 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include <stdlib.h>
+
+typedef struct s_LLNode {
+    int *data;
+    struct s_LLNode *next;
+} LLNode;
+
+typedef struct s_LinkedList {
+    LLNode *head;
+    int len;
+} LinkedList;
+
+LinkedList linked_list() {
+    LinkedList list = {
+        .head = NULL,
+        .len = 0,
+    };
+    return list;
+}
+
+void add_to_list(LinkedList *list, int data) {
+    LLNode *node;
+    for (node = list->head; node != NULL; node = node->next); // move to last node
+    node = (LLNode*) malloc(sizeof(LLNode));
+    node->data = (int*) malloc(sizeof(data));
+    node->next = NULL;
+}
+
+#endif
